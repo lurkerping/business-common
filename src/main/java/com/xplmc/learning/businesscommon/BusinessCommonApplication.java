6f54460a61a3e994e271c8a42af3281ee5b12589
xiaoping <lurkerping@gmail.com>
2017-12-13 21:46:40 +0800

    initial commit

---
+++ src/main/java/com/xplmc/learning/businesscommon/BusinessCommonApplication.java
@@ -0,0 +1,12 @@
+package com.xplmc.learning.businesscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCommonApplication.class, args);
+	}
+}
