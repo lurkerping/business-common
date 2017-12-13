60739a1f9373a625dcb5b51805c96e0d16faf7a0
xiaoping <lurkerping@gmail.com>
2017-12-13 22:31:36 +0800

    import guava

---
+++ src/main/java/com/xplmc/learning/businesscommon/locking/KeyLock.java
@@ -0,0 +1,7 @@
+package com.xplmc.learning.businesscommon.locking;
+
+/**
+ * fine-grained locking with application specific keys
+ */
+public class KeyLock {
+}
---
+++ src/main/java/com/xplmc/learning/businesscommon/locking/KeyLockManager.java
@@ -0,0 +1,7 @@
+package com.xplmc.learning.businesscommon.locking;
+
+/**
+ * KeyLockManager using Guava's Cache to manage Lock Object
+ */
+public class KeyLockManager {
+}
